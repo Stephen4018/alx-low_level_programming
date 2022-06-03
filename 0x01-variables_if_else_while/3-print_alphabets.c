57f07471268cdc2a3edd7af42d0867bbf8da242a
Stephen4018 <60546277+Stephen4018@users.noreply.github.com>
2022-06-03 23:28:49 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,27 @@
-aa
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: alphabets in lower and upper
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
